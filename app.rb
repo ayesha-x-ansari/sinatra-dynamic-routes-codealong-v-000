f6b930f12f5f78a8cf7839dc31015937eab7fd2e
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-24 02:00:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,9 +19,9 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "/multiply/:num1/:num2" do
-    @answer = params[:num1]  * params[:num2]
-    " #{@answer}"
-  end
+#  get "/multiply/:num1/:num2" do
+#    @answer = params[:num1]  * params[:num2]
+#    " #{@answer}"
+#  end
 
 end
